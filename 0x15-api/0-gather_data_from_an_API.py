a59b239e255fde59a2554d2ac8e1fe9c4efc681b
ZainAlabidin <zynly9303@gmail.com>
2024-06-04 06:12:18 +0300

    ok

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -31,8 +31,7 @@ if __name__ == "__main__":
     number_of_done_tasks = len(completed_tasks)
 
     print(
-        f"Employee {employee_name} is done
-          with tasks({number_of_done_tasks}/{total_tasks}):"
+        f"Employee {employee_name} is done with tasks({number_of_done_tasks}/{total_tasks}):"
     )
     for task in completed_tasks:
         print(f"\t {task.get('title')}")
