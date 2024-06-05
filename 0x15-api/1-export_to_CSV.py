461d7d1a918045ebaeda4f071a571bff2779d573
ZainAlabidin <zynly9303@gmail.com>
2024-06-06 01:41:06 +0300

    ok

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python3
 """
-Python script that, using this REST API,
-for a given employee ID, returns information
-about his/her TODO list progress.
+Python script to export data in the CSV format.
 """
 
 import requests
@@ -14,23 +12,15 @@ if __name__ == "__main__":
 
     user_id = int(sys.argv[1])
 
-    # Base URLs for the API
-    users_url = f"https://jsonplaceholder.typicode.com/users/{user_id}"
-    todos_url = f"https://jsonplaceholder.typicode.com/todos?userId={user_id}"
-
-    user_response = requests.get(users_url)
-
-    user_data = user_response.json()
-    user_name = user_data.get("name")
-
-    todos_response = requests.get(todos_url)
-
-    todos_data = todos_response.json()
+    url = "https://jsonplaceholder.typicode.com/"
+    user_data = requests.get(url + "users/{}".format(user_id)).json()
+    todos_data = requests.get(url + "todos", params={"userId": user_id}).json()
+    username = user_data.get("username")
     csv_filename = f"{user_id}.csv"
 
     with open(csv_filename, mode="w", newline="") as file:
         writer = csv.writer(file, quoting=csv.QUOTE_ALL)
         for task in todos_data:
             writer.writerow(
-                [user_id, user_name, task.get("completed"), task.get("title")]
+                [user_id, username, task.get("completed"), task.get("title")]
             )
