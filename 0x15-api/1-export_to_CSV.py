d0d29af0d3e65124cb7a7e0f573b263023e8aa7d
ZainAlabidin <zynly9303@gmail.com>
2024-06-06 01:42:19 +0300

    ok

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -16,7 +16,7 @@ if __name__ == "__main__":
     user_data = requests.get(url + "users/{}".format(user_id)).json()
     todos_data = requests.get(url + "todos", params={"userId": user_id}).json()
     username = user_data.get("username")
-    csv_filename = f"{user_id}.csv"
+    csv_filename = "{}.csv".format(user_id)
 
     with open(csv_filename, mode="w", newline="") as file:
         writer = csv.writer(file, quoting=csv.QUOTE_ALL)
