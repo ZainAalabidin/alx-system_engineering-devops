2892875eae95222ea5f8f56b74d530fb50406fdf
ZainAlabidin <zynly9303@gmail.com>
2024-06-06 02:11:39 +0300

    ok

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -17,6 +17,8 @@ if __name__ == "__main__":
 
     with open(csv_filename, mode="w", newline="") as file:
         writer = csv.writer(file, quoting=csv.QUOTE_ALL)
+        writer.writerow(["USER_ID", "USERNAME", "TASK_COMPLETED_STATUS", "TASK_TITLE"])
+        
         for task in todos_data:
             writer.writerow(
                 [user_id, username, task.get("completed"), task.get("title")]
